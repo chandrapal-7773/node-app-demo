c7c648caf7060723963f1710da42920b1290c501
chandrapal-7773 <166910560+chandrapal-7773@users.noreply.github.com>
2024-05-26 21:07:59 +1000

    Update install.sh

--- deploy_scripts/install.sh
+++ deploy_scripts/install.sh
@@ -1,6 +1,6 @@
 # add nodejs to yum
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash -
-yum install nodejs -y #default-jre ImageMagick
+apt install nodejs -y #default-jre ImageMagick
 
 # install pm2 module globaly
 npm install -g pm2
